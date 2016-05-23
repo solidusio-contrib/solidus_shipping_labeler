9acffa08bba4c2379240cf2edbe011460da555b4
Jason Fleetwood-Boldt <jason@datatravels.com>
2016-05-22 22:05:48 -0400

    changes syntax

--- app/overrides/spree/admin/return_authorizations/edit/add_return_labels_to_rma_edit.rb
+++ app/overrides/spree/admin/return_authorizations/edit/add_return_labels_to_rma_edit.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   :virtual_path => "spree/admin/return_authorizations/edit",
   :name => "add_return_labels_to_rma_edit",
   # :insert_after => "erb[loud]:contains('form_for'):contains('@return_authorization')",
-  :insert_after => "[data-hook=bottom-of-return-authorization-edit]",
+  :insert_after => "[data-hook=below-return-authorization-form]",
   :partial => "spree/admin/return_authorizations/return_shipping_labels"
 )
