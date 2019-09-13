37d548ecf260d5c5c3d1792b0acc5a55b96f19a6
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-13 11:41:20 +0200

    Use solidus_support to configure the test environment

--- lib/solidus_shipping_labeler.rb
+++ lib/solidus_shipping_labeler.rb
@@ -1,3 +1,7 @@
 require 'spree/core'
+require 'solidus_support'
+
 require 'solidus_shipping_labeler/engine'
 require 'solidus_shipping_labeler/utilities'
+
+SolidusShippingLabeler = SpreeShippingLabeler
