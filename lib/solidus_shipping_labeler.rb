505af8d00c60345bf2c0f10202471867db9099b9
Alessandro Desantis <desa.alessandro@gmail.com>
2020-01-17 13:46:37 +0100

    Upgrade infrastructure with solidus_dev_support

--- lib/solidus_shipping_labeler.rb
+++ lib/solidus_shipping_labeler.rb
@@ -4,5 +4,7 @@ require 'solidus_core'
 require 'solidus_support'
 require 'deface'
 
+require 'solidus_shipping_labeler/version'
 require 'solidus_shipping_labeler/engine'
 require 'solidus_shipping_labeler/utilities'
+require 'solidus_shipping_labeler/fed_ex_connection'
--- lib/solidus_shipping_labeler/engine.rb
+++ lib/solidus_shipping_labeler/engine.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 
+require 'spree/core'
+
 module SolidusShippingLabeler
   class Engine < Rails::Engine
+    include SolidusSupport::EngineExtensions::Decorators
+
+    isolate_namespace Spree
+
     engine_name 'solidus_shipping_labeler'
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/models/*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
+    # use rspec for tests
+    config.generators do |g|
+      g.test_framework :rspec
     end
-
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += %W(#{config.root}/lib/utilities)
-    config.to_prepare &method(:activate).to_proc
   end
 end
